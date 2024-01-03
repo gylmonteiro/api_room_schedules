29384fafde7138f4a46e60695c0167d7ebf8d99e
Gyl Monteiro <gyl.tecno.especialista@gmail.com>
2024-01-03 15:33:29 -0300

    feat: correction in url to information date schedules

--- schedules/urls.py
+++ schedules/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [path('times/', views.TimeListCreateView.as_view(), name='list-cre
                path('places/<int:pk>/', views.PlaceRetrieveUpdateDestroyView.as_view(), name='retrieve-update-destroy-place'),
                path('schedules/', views.ScheduleListCreateView.as_view(), name='list-create-schedules'),
                path('schedules/<int:pk>/', views.ScheduleRetrieveUpdateDestroyView.as_view(), name='retrieve-update-destroy-schedule'),
-               path('schedules/date/<str:date>/', views.AllSchedulesForDateView.as_view(), name='list_schedules_confirmations',),
+               path('schedules/<str:date>/', views.AllSchedulesForDateView.as_view(), name='list_schedules_confirmations',),
                path('schedules/<int:user_id>/<str:date>/', views.AllSchedulesForUserWithDateView.as_view(), name='list_schedules_for_user_with_date')]
