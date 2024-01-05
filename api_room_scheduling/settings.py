af5d27b1cc07de571b8d2d3de98887d7d49f7055
Gyl Monteiro <gyl.tecno.especialista@gmail.com>
2024-01-05 15:07:47 -0300

    feat: config allowed host

--- api_room_scheduling/settings.py
+++ api_room_scheduling/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5(w%jcw4o-s*3k5uc@q+za%d@k*%467p6+eh+k4$@q5)s#n-bh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
